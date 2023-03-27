e5b1e2dc764bdd467ed5eb1cbc2a09f33175b95f
handi7 <handi7.co@gmail.com>
2023-03-27 14:14:42 +0700

    Add firebase config to env variable

--- config/fire.js
+++ config/fire.js
@@ -1,14 +1,15 @@
+require("dotenv").config();
 const { initializeApp } = require("firebase/app");
 const { getStorage } = require("firebase/storage");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAX3kPM8DtQwam7qjhC3OUKAIrqaG_fOCo",
-  authDomain: "pos-toko-15cb4.firebaseapp.com",
-  projectId: "pos-toko-15cb4",
-  storageBucket: "pos-toko-15cb4.appspot.com",
-  messagingSenderId: "895561848842",
-  appId: "1:895561848842:web:7baacce1122ceb799a27f3",
-  measurementId: "G-4BFE423JGD",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
